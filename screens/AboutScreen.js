f2b4e81c07ad23acb53275e890374ab1728ab4d4
mroberts1137 <mroberts1137@gmail.com>
2024-02-14 15:54:21 -0500

    Animatable

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -4,6 +4,7 @@ import { Avatar, ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from '../components/LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const Mission = () => {
   return (
@@ -41,35 +42,39 @@ const AboutScreen = () => {
   if (partners.errMess) {
     return (
       <ScrollView>
-        <Mission />
-        <Card>
-          <Card.Title>Community Partners</Card.Title>
-          <Card.Divider />
-          <Text>{partners.errMess}</Text>
-        </Card>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Mission />
+          <Card>
+            <Card.Title>Community Partners</Card.Title>
+            <Card.Divider />
+            <Text>{partners.errMess}</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
 
   return (
     <ScrollView>
-      <Mission />
+      <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+        <Mission />
 
-      <Card>
-        <Card.Title>Community Partners</Card.Title>
-        <Card.Divider />
-        {partners.partnersArray.map((partner) => {
-          return (
-            <ListItem key={partner.id}>
-              <Avatar rounded source={{ uri: baseUrl + partner.image }} />
-              <ListItem.Content>
-                <ListItem.Title>{partner.name}</ListItem.Title>
-                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-              </ListItem.Content>
-            </ListItem>
-          );
-        })}
-      </Card>
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          {partners.partnersArray.map((partner) => {
+            return (
+              <ListItem key={partner.id}>
+                <Avatar rounded source={{ uri: baseUrl + partner.image }} />
+                <ListItem.Content>
+                  <ListItem.Title>{partner.name}</ListItem.Title>
+                  <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                </ListItem.Content>
+              </ListItem>
+            );
+          })}
+        </Card>
+      </Animatable.View>
     </ScrollView>
   );
 };
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { postComment } from '../features/comments/commentsSlice';
+import * as Animatable from 'react-native-animatable';
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
@@ -52,83 +53,85 @@ const CampsiteInfoScreen = ({ route }) => {
   };
 
   return (
-    <FlatList
-      data={comments.commentsArray.filter(
-        (comment) => comment.campsiteId === campsite.id
-      )}
-      renderItem={renderCommentItem}
-      keyExtractor={(item) => item.id.toString()}
-      contentContainerStyle={{
-        marginHorizontal: 20,
-        paddingVertical: 20
-      }}
-      ListHeaderComponent={
-        <>
-          <RenderCampsite
-            campsite={campsite}
-            isFavorite={favorites.includes(campsite.id)}
-            markFavorite={() => dispatch(toggleFavorite(campsite.id))}
-            onShowModal={() => setShowModal(!showModal)}
-          />
-          <Text style={styles.commentsTitle}>Comments</Text>
+    <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+      <FlatList
+        data={comments.commentsArray.filter(
+          (comment) => comment.campsiteId === campsite.id
+        )}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{
+          marginHorizontal: 20,
+          paddingVertical: 20
+        }}
+        ListHeaderComponent={
+          <>
+            <RenderCampsite
+              campsite={campsite}
+              isFavorite={favorites.includes(campsite.id)}
+              markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+              onShowModal={() => setShowModal(!showModal)}
+            />
+            <Text style={styles.commentsTitle}>Comments</Text>
 
-          <Modal
-            animationType='slide'
-            transparent={false}
-            visible={showModal}
-            onRequestClose={() => setShowModal(!showModal)}
-          >
-            <View style={styles.modal}>
-              <Rating
-                showRating
-                startingValue={rating}
-                imageSize={40}
-                onFinishRating={(rating) => setRating(rating)}
-                style={{ paddingVertical: 10 }}
-              />
-
-              <Input
-                placeholder={'Author'}
-                leftIcon={{ type: 'font-awesome', name: 'user-o' }}
-                leftIconContainerStyle={{ paddingRight: 10 }}
-                onChangeText={(author) => setAuthor(author)}
-                value={author}
-              />
-
-              <Input
-                placeholder={'Comment'}
-                leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
-                leftIconContainerStyle={{ paddingRight: 10 }}
-                onChangeText={(comment) => setText(comment)}
-                value={text}
-              />
-
-              <View style={{ margin: 10 }}>
-                <Button
-                  title='Submit'
-                  onPress={() => {
-                    handleSubmit();
-                    resetForm();
-                  }}
-                  color='#5637DD'
+            <Modal
+              animationType='slide'
+              transparent={false}
+              visible={showModal}
+              onRequestClose={() => setShowModal(!showModal)}
+            >
+              <View style={styles.modal}>
+                <Rating
+                  showRating
+                  startingValue={rating}
+                  imageSize={40}
+                  onFinishRating={(rating) => setRating(rating)}
+                  style={{ paddingVertical: 10 }}
                 />
-              </View>
 
-              <View style={{ margin: 10 }}>
-                <Button
-                  title='Cancel'
-                  onPress={() => {
-                    setShowModal(!showModal);
-                    resetForm();
-                  }}
-                  color='#808080'
+                <Input
+                  placeholder={'Author'}
+                  leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                  leftIconContainerStyle={{ paddingRight: 10 }}
+                  onChangeText={(author) => setAuthor(author)}
+                  value={author}
                 />
+
+                <Input
+                  placeholder={'Comment'}
+                  leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                  leftIconContainerStyle={{ paddingRight: 10 }}
+                  onChangeText={(comment) => setText(comment)}
+                  value={text}
+                />
+
+                <View style={{ margin: 10 }}>
+                  <Button
+                    title='Submit'
+                    onPress={() => {
+                      handleSubmit();
+                      resetForm();
+                    }}
+                    color='#5637DD'
+                  />
+                </View>
+
+                <View style={{ margin: 10 }}>
+                  <Button
+                    title='Cancel'
+                    onPress={() => {
+                      setShowModal(!showModal);
+                      resetForm();
+                    }}
+                    color='#808080'
+                  />
+                </View>
               </View>
-            </View>
-          </Modal>
-        </>
-      }
-    />
+            </Modal>
+          </>
+        }
+      />
+    </Animatable.View>
   );
 };
 
